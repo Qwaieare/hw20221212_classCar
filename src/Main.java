fa95254061547de3790298d2e905ca1cebc7a5f0
Qwaieare <gb-grand@mail.ru>
2022-12-17 04:13:29 +0300

    hw20221212_classCar is done

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,19 @@
+import java.util.*;
+import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+     Car car1 = new Car("Lada Grande", " ", 2015, "Россия", "желтый", 1.7);
+     Car car2 = new Car("Audi A8", "50 L TDI quattro", 2020, "Германия", "черный", 3.0);
+     Car car3 = new Car("BMW", "Z8", 2021, "Германия", "черный", 3.0);
+     Car car4 = new Car("Kia", "Sportage 4 поколение", 2018, "Ю.Корея", "красный", 2.4);
+     Car car5 = new Car("Hyundai", "Avante", 2016, "Ю.Корея", "оранжевый", 1.6);
+
+        System.out.println(car1);
+        System.out.println(car2);
+        System.out.println(car3);
+        System.out.println(car4);
+        System.out.println(car5);
     }
+
 }
